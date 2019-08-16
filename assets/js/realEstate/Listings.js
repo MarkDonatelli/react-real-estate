cd253ed0dde63c6e3e5bc52a3c9088475dc53828
Mark Donatelli <markwdonatelli@yahoo.com>
2019-08-16 12:38:11 -0400

    updated content area styles

--- assets/js/realEstate/Listings.js
+++ assets/js/realEstate/Listings.js
@@ -1,81 +1,197 @@
-import React, { Component} from 'react'
-
-
+import React, { Component } from "react";
 
 export default class Listings extends Component {
-  constructor () {
-    super()
+  constructor() {
+    super();
     this.state = {
-      name: 'Mark'
-    }
+      name: "Mark"
+    };
   }
- 
-  render () {
-    return (<section id="listings">
-    <section className="search-area">
-      <input type="text" name="search"/>
-    </section>
 
-    <section className="sortby-area">
-    <div className="results">390 results found</div>
-    <div className="sort-options">
-    <select name="sortby" className="sortby">
-    <option value="price-asc">Highest Price</option>
-    <option value="price-asc">Lowest Price</option>
-    </select>
-    <div className="view">
-    <i className="fa fa-th-list" aria-hidden="true"></i>
-    <i className="fa fa-th" aria-hidden="true"></i>
-    </div>
-    </div>
-    </section>
+  render() {
+    return (
+      <section id="listings">
+        <section className="search-area">
+          <input type="text" name="search" />
+        </section>
 
-    <section className="listings-results">
-    <div className="listing">
-      <div className="listing-img">
-      <span className="address">75 Temple Ave</span>
-      <div className="details">
-      <div className="col-md-3">
-      <div className="user-img"></div>
-      </div>
+        <section className="sortby-area">
+          <div className="results">390 results found</div>
+          <div className="sort-options">
+            <select name="sortby" className="sortby">
+              <option value="price-asc">Highest Price</option>
+              <option value="price-asc">Lowest Price</option>
+            </select>
+            <div className="view">
+              <i className="fa fa-th-list" aria-hidden="true" />
+              <i className="fa fa-th" aria-hidden="true" />
+            </div>
+          </div>
+        </section>
 
-      <div className="col-md-9">
-      <div className="user-details">
-        <span className="user-name">Bill Gates</span>
-        <span className="post-date">05/08/19</span>
-      </div>
-      <div className="listing-details">
-      <div className="floor-space"><i className="fa fa-square-o" aria-hidden="true"></i> <span>1000 ft&sup2;</span></div>
-      <div className="bedrooms"><i className="fa fa-bed" aria-hidden="true"></i><span>3 bedrooms</span></div>
-      </div>
-      <div className="view-btn">
-        View Listing
-      </div>
-      </div>
+        <section className="listings-results">
+          <div className="col-md-3">
+            <div className="listing">
+              <div className="listing-img">
+                <span className="address">75 Temple Ave</span>
+                <div className="details">
+                  <div className="col-md-3">
+                    <div className="user-img" />
+                  </div>
 
-      </div>
-      </div>
-      <div className="bottom-info">
-      <span>$1000 / month</span>
-      <span><i className="fa fa-map-marker" aria-hidden="true"></i>Ridgewood, NY</span>
-      </div>
-    </div>
-    </section>
+                  <div className="col-md-9">
+                    <div className="user-details">
+                      <span className="user-name">Mark Donatelli</span>
+                      <span className="post-date">05/08/19</span>
+                    </div>
+                    <div className="listing-details">
+                      <div className="floor-space">
+                        <i className="fa fa-square-o" aria-hidden="true" />{" "}
+                        <span>1000 ft&sup2;</span>
+                      </div>
+                      <div className="bedrooms">
+                        <i className="fa fa-bed" aria-hidden="true" />
+                        <span>3 bedrooms</span>
+                      </div>
+                    </div>
+                    <div className="view-btn">View Listing</div>
+                  </div>
+                </div>
+              </div>
+              <div className="bottom-info">
+                <span className="price">$1000 / month</span>
+                <span className="location">
+                  <i className="fa fa-map-marker" aria-hidden="true" />
+                  Ridgewood, NY
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="listing">
+              <div className="listing-img">
+                <span className="address">75 Temple Ave</span>
+                <div className="details">
+                  <div className="col-md-3">
+                    <div className="user-img" />
+                  </div>
 
+                  <div className="col-md-9">
+                    <div className="user-details">
+                      <span className="user-name">Mark Donatelli</span>
+                      <span className="post-date">05/08/19</span>
+                    </div>
+                    <div className="listing-details">
+                      <div className="floor-space">
+                        <i className="fa fa-square-o" aria-hidden="true" />{" "}
+                        <span>1000 ft&sup2;</span>
+                      </div>
+                      <div className="bedrooms">
+                        <i className="fa fa-bed" aria-hidden="true" />
+                        <span>3 bedrooms</span>
+                      </div>
+                    </div>
+                    <div className="view-btn">View Listing</div>
+                  </div>
+                </div>
+              </div>
+              <div className="bottom-info">
+                <span className="price">$1000 / month</span>
+                <span className="location">
+                  <i className="fa fa-map-marker" aria-hidden="true" />
+                  Ridgewood, NY
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="listing">
+              <div className="listing-img">
+                <span className="address">75 Temple Ave</span>
+                <div className="details">
+                  <div className="col-md-3">
+                    <div className="user-img" />
+                  </div>
 
-    <section className="pagination">
-      <ul className="pagination-numbers">
-      <li>Prev</li>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
-      <li>5</li>
-      <li>Next</li>
-      </ul>
-    </section>
+                  <div className="col-md-9">
+                    <div className="user-details">
+                      <span className="user-name">Mark Donatelli</span>
+                      <span className="post-date">05/08/19</span>
+                    </div>
+                    <div className="listing-details">
+                      <div className="floor-space">
+                        <i className="fa fa-square-o" aria-hidden="true" />{" "}
+                        <span>1000 ft&sup2;</span>
+                      </div>
+                      <div className="bedrooms">
+                        <i className="fa fa-bed" aria-hidden="true" />
+                        <span>3 bedrooms</span>
+                      </div>
+                    </div>
+                    <div className="view-btn">View Listing</div>
+                  </div>
+                </div>
+              </div>
+              <div className="bottom-info">
+                <span className="price">$1000 / month</span>
+                <span className="location">
+                  <i className="fa fa-map-marker" aria-hidden="true" />
+                  Ridgewood, NY
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="listing">
+              <div className="listing-img">
+                <span className="address">75 Temple Ave</span>
+                <div className="details">
+                  <div className="col-md-3">
+                    <div className="user-img" />
+                  </div>
 
-    </section>)
+                  <div className="col-md-9">
+                    <div className="user-details">
+                      <span className="user-name">Mark Donatelli</span>
+                      <span className="post-date">05/08/19</span>
+                    </div>
+                    <div className="listing-details">
+                      <div className="floor-space">
+                        <i className="fa fa-square-o" aria-hidden="true" />{" "}
+                        <span>1000 ft&sup2;</span>
+                      </div>
+                      <div className="bedrooms">
+                        <i className="fa fa-bed" aria-hidden="true" />
+                        <span>3 bedrooms</span>
+                      </div>
+                    </div>
+                    <div className="view-btn">View Listing</div>
+                  </div>
+                </div>
+              </div>
+              <div className="bottom-info">
+                <span className="price">$1000 / month</span>
+                <span className="location">
+                  <i className="fa fa-map-marker" aria-hidden="true" />
+                  Ridgewood, NY
+                </span>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="pagination">
+          <ul className="pagination-numbers">
+            <li>Prev</li>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+            <li>Next</li>
+          </ul>
+        </section>
+      </section>
+    );
   }
 }
-
